d22cea2f0ae0625c4120eee9b2adb05c99577642
Evan Meade <evan.meade@protonmail.com>
2021-01-01 15:43:56 -0600

    Pages plots

--- graphics.r
+++ graphics.r
@@ -1,8 +1,15 @@
 # Import libraries
 library(ggplot2)
+library(RColorBrewer)
+library(ggthemes)
 
 # Reading in the data
 books <- read.csv("books_read.csv")
+
+
+# 
+# Exploration of the data
+# 
 
 # Basic statistics
 summary(books)
@@ -44,7 +51,52 @@ pages_line <- ggplot(data = books) +
 print(pages_line)
 
 
+# Pages per author
+pages_bar <- ggplot(data = books) +
+  geom_bar(mapping = aes(x = author, y = pages), stat = "identity") +
+  labs(title = "Pages Read by Author")
+print(pages_bar)
+
+# Percent of pages done by each author
+author_bar <- ggplot(data = books) +
+  geom_bar(mapping = aes(x = "Author", y = pages, fill = author),
+           stat = "identity",
+           position = "fill") +
+  labs(title = "Authors' Proportions of Pages Read")
+print(author_bar)
 
 
+# 
+# Final Plots
+# 
 
+# Simplified percent of pages done by each author
+author_pages <- aggregate(pages ~ author, books, sum)
+author_pages <- author_pages[order(author_pages$pages, decreasing = TRUE), ]
+rownames(author_pages) <- 1:nrow(author_pages)
+simp_author_pages <- author_pages[1:5, ]
+simp_author_pages[6, "author"] <- "Other"
+simp_author_pages[6, "pages"] <- sum(author_pages[6:nrow(author_pages), "pages"])
+simp_author_pages$author <- factor(simp_author_pages$author, levels = simp_author_pages$author)
+simp_author_bar <- ggplot(data = simp_author_pages) +
+  theme_fivethirtyeight(base_size = 24) +
+  geom_bar(mapping = aes(x = "", y = pages, fill = author),
+           stat = "identity") +
+  labs(title = "Total Pages Read, Segmented by Author",
+       y = "Pages Read") +
+  scale_fill_manual(name = "Author", values = rev(brewer.pal(6, "Spectral"))) +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+print(simp_author_bar)
 
+# Cleaned up histogram of page length
+page_hist_clean <- ggplot(data = books) +
+  theme_fivethirtyeight(base_size = 24) +
+  geom_histogram(mapping = aes(x = pages, fill = "orange"),
+                 binwidth = 200, boundary = 0, closed = "left") +
+  scale_x_continuous(breaks = seq(0, 1000, 200)) +
+  scale_y_continuous(breaks = seq(0, 18, 3)) +
+  labs(title = "Distribution of Book Page Lengths") +
+  theme(legend.position = "none")
+print(page_hist_clean)
